498149bfd9abbbc3525f7a0b390ae11df471da16
Kenneth Torng <ktorng@gmail.com>
2018-03-10 05:51:30 +0000

    Clean up slack events tutorial code

--- index.js
+++ index.js
@@ -1,94 +1,16 @@
-// Load environment variables
 require('dotenv').config();
 
-const slackEventsApi = require('@slack/events-api');
-const passport = require('passport');
-const SlackStrategy = require('@aoberoi/passport-slack').default.Strategy;
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const { getClientByTeamId } = require('./util/getClient');
+const slackEvents = require('./slackEvents');
 
 const port = process.env.APP_PORT || 3000;
-
-// Initialize a data structures to store team authorization info (typically stored in a database)
-const botAuthorizations = {}
-
-// *** Initialize event adapter using verification token from environment variables ***
-const slackEvents = slackEventsApi.createSlackEventAdapter(
-  process.env.SLACK_VERIFICATION_TOKEN, 
-  { includeBody: true }
-);
-
-// Initialize Add to Slack (OAuth) helpers
-passport.use(new SlackStrategy({
-  clientID: process.env.SLACK_CLIENT_ID,
-  clientSecret: process.env.SLACK_CLIENT_SECRET,
-  skipUserProfile: true,
-}, (accessToken, scopes, team, extra, profiles, done) => {
-  botAuthorizations[team.id] = extra.bot.accessToken;
-  done(null, {});
-}));
-
-// Initialize an Express application
 const app = express();
+
 app.use(bodyParser.json());
-
-// Plug the Add to Slack (OAuth) helpers into the express app
-app.use(passport.initialize());
-app.get('/', (req, res) => {
-  res.send('<a href="/auth/slack"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
-});
-app.get('/auth/slack', passport.authenticate('slack', {
-  scope: ['bot']
-}));
-app.get('/auth/slack/callback',
-  passport.authenticate('slack', { session: false }),
-  (req, res) => {
-    res.send('<p>Greet and React was successfully installed on your team.</p>');
-  },
-  (err, req, res, next) => {
-    res.status(500).send(`<p>Greet and React failed to install</p> <pre>${err}</pre>`);
-  }
-);
-
-// *** Plug the event adapter into the express app as middleware ***
 app.use('/slack/events', slackEvents.expressMiddleware());
 
-// *** Attach listeners to the event adapter ***
-
-// *** Greeting any user that says "hi" ***
-slackEvents.on('message', (message, body) => {
-  // Only deal with messages that have no subtype (plain messages) and contain 'hi'
-  if (!message.subtype && message.text.indexOf('hi') >= 0) {
-    // Initialize a client
-    const slack = getClientByTeamId(body.team_id, botAuthorizations);
-    // Handle initialization failure
-    if (!slack) {
-      return console.error('No authorization found for this team. Did you install this app again after restarting?');
-    }
-    // Respond to the message back in the same channel
-    slack.chat.postMessage({ 
-      channel: message.channel, 
-      text: `B-b-baka <@${message.user}>! :blushkek:` 
-    })
-      .catch(console.error);
-  }
-});
-
-// *** Handle errors ***
-slackEvents.on('error', (error) => {
-  if (error.code === slackEventsApi.errorCodes.TOKEN_VERIFICATION_FAILURE) {
-    // This error type also has a `body` propery containing the request body which failed verification.
-    console.error(`An unverified request was sent to the Slack events Request URL. Request body: \
-${JSON.stringify(error.body)}`);
-  } else {
-    console.error(`An error occurred while handling a Slack event: ${error.message}`);
-  }
-});
-
-// Start the express application
 http.createServer(app).listen(port, () => {
-  console.log(`server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
---
+++ slackEvents.js
@@ -0,0 +1,28 @@
+const slackEventsApi = require('@slack/events-api');
+const { WebClient } = require('@slack/client');
+
+const slackEvents = slackEventsApi.createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN, { includeBody: true });
+
+slackEvents.on('message', (message, body) => {
+  if (!message.subtype && message.text.indexOf('hi') >= 0) {
+    const slack = new WebClient(process.env.SAPPHIRE_OAUTH_TOKEN);
+    
+    if (slack) {
+      slack.chat.postMessage({ 
+        channel: message.channel, 
+        text: `B-b-baka <@${message.user}>! :blushkek:` 
+      })
+        .catch(console.error);
+    }
+  }
+});
+
+slackEvents.on('error', (error) => {
+  if (error.code === slackEventsApi.errorCodes.TOKEN_VERIFICATION_FAILURE) {
+    console.error(`An unverified request was sent to the Slack events Request URL. Request body: ${JSON.stringify(error.body)}`);
+  } else {
+    console.error(`An error occurred while handling a Slack event: ${error.message}`);
+  }
+});
+
+module.exports = slackEvents;
